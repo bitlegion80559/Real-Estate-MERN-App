3155fb209d56d7bde1031a13ed47035025acd040
Umesh <umeshnagar9152@gmail.com>
2024-04-16 22:35:52 +0530

    Created listing

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js';
 import connectDB from './db/connectDb.js';
 dotenv.config();
 const app=express();
@@ -25,6 +26,7 @@ app.listen(process.env.PORT,()=>{
 // routes
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
