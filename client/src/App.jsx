5a44cae7dbff63814d85784d90686599b955496a
Umesh <umeshnagar9152@gmail.com>
2024-04-16 23:03:36 +0530

    Ui of listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import CreateListing from './pages/CreateListing';
 import About from './pages/About'
 import Header from './components/Header'
 import ContactUs from './pages/ContactUs'
@@ -20,8 +21,8 @@ const App = () => {
         <Route path="/contact-us" element={<ContactUs/>}/>
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
-          {/* <Route path='/create-listing' element={<CreateListing />} />
-          <Route
+          <Route path='/create-listing' element={<CreateListing />} />
+          {/* <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
           /> */}
