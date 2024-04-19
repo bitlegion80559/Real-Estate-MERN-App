95d543ff28d37df41590d9d05089d2da466632be
Umesh <umeshnagar9152@gmail.com>
2024-04-20 02:34:24 +0530

    added some pages

--- client/src/components/WentWrong.jsx
+++ client/src/components/WentWrong.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const WentWrong = () => {
   return (
-    <div>WentWrong</div>
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <img src='/wentwrong.jpg' alt='photo' className='max-w-full h-auto'/>
+      <h2 className='font-bold text-3xl'>Something Went Wrong </h2>
+    </div>
   )
 }
 
